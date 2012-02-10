060089558b0408351ddbd208aa230a79f03764aa
Andriy Gerasika <andriy.gerasika@gmail.com>
2012-02-10 18:30:33 +0000

    sax chain established

---
+++ src/com/gerixsoft/xslt4json/JsonSaxSerializer.java
@@ -0,0 +1,83 @@
+package com.gerixsoft.xslt4json;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+public class JsonSaxSerializer implements ContentHandler {
+
+	private OutputStream outputStream;
+	
+	private void write(String s) throws SAXException {
+		try {
+			outputStream.write(s.getBytes());
+		} catch (IOException e) {
+			throw new SAXException(e);
+		}
+	}
+	
+	public JsonSaxSerializer(OutputStream outputStream) {
+		this.outputStream = outputStream;
+	}
+	
+	@Override
+	public void setDocumentLocator(Locator locator) {
+		
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		write("// start of json\n");
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		write("// end of json\n");
+	}
+
+	@Override
+	public void startPrefixMapping(String prefix, String uri) throws SAXException {
+		
+	}
+
+	@Override
+	public void endPrefixMapping(String prefix) throws SAXException {
+		
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes atts)
+			throws SAXException {
+		
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		
+	}
+
+	@Override
+	public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+		
+	}
+
+	@Override
+	public void processingInstruction(String target, String data) throws SAXException {
+		
+	}
+
+	@Override
+	public void skippedEntity(String name) throws SAXException {
+		
+	}
+
+}
--- src/com/gerixsoft/xslt4json/JsonToXml.java
+++ src/com/gerixsoft/xslt4json/JsonToXml.java
@@ -1,17 +1,18 @@
 package com.gerixsoft.xslt4json;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
+import javax.xml.validation.ValidatorHandler;
 
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
@@ -22,24 +23,28 @@ public class JsonToXml {
 
 	public static void main(String[] args) throws IOException, SAXException, TransformerException {
 		if (args.length != 2) {
-			System.err.println("usage: <json-file> <xml-file>");
+			System.err.println("usage: <input-file> <output-file>");
 			System.exit(-1);
 		}
-		File jsonFile = new File(args[0]);
-		File xmlFile = new File(args[1]);
+		File inputFile = new File(args[0]);
+		File outputFile = new File(args[1]);
 
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		transformer.transform(
-				new SAXSource(new JsonSaxParser(), new InputSource(jsonFile.toString())),
-				new StreamResult(xmlFile));
-
+		JsonSaxSerializer writer = new JsonSaxSerializer(new FileOutputStream(outputFile));
+		
 		SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
 		Schema schema = schemaFactory.newSchema(new StreamSource(JsonToXml.class
 				.getResourceAsStream("json.xsd")));
-		Validator validator = schema.newValidator();
-		validator.setErrorHandler(new __ErrorHandler());
-		validator.validate(new StreamSource(xmlFile));
-
+		ValidatorHandler validatorHandler = schema.newValidatorHandler();
+		validatorHandler.setErrorHandler(new __ErrorHandler());
+		validatorHandler.setContentHandler(writer);
+		
+		TransformerHandler transformerHandler = ((SAXTransformerFactory)TransformerFactory.newInstance()).newTransformerHandler();
+		transformerHandler.setResult(new SAXResult(validatorHandler));
+		
+		JsonSaxParser reader = new JsonSaxParser();
+		reader.setContentHandler(transformerHandler);
+		reader.parse(new InputSource(inputFile.toString()));
+		
 		System.out.println("ok");
 	}
 
